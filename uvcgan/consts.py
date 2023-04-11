d937cb055c530956b7aa4955da1d0b2fe93bcf25
Dmitrii Torbunov <dtorbunov@bnl.gov>
2023-04-11 13:19:35 -0400

    consts: Sync from uvcgan2 v0.0.1

--- uvcgan/consts.py
+++ uvcgan/consts.py
@@ -4,3 +4,13 @@ CONFIG_NAME = 'config.json'
 ROOT_DATA   = os.environ.get('UVCGAN_DATA',   'data')
 ROOT_OUTDIR = os.environ.get('UVCGAN_OUTDIR', 'outdir')
 
+SPLIT_TRAIN = 'train'
+SPLIT_VAL   = 'val'
+SPLIT_TEST  = 'test'
+
+MERGE_PAIRED   = 'paired'
+MERGE_UNPAIRED = 'unpaired'
+MERGE_NONE     = 'none'
+
+MODEL_STATE_TRAIN = 'train'
+MODEL_STATE_EVAL  = 'eval'
